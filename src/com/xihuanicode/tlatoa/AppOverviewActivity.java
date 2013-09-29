90093ad98497b741eeb9bd6fc8f19047b0f4abb0
Jorge Eduardo <hdez.jeduardo@gmail.com>
2013-09-29 17:43:55 -0500

    Adding first source version

---
+++ src/com/xihuanicode/tlatoa/AppOverviewActivity.java
@@ -0,0 +1,15 @@
+package com.xihuanicode.tlatoa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppOverviewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.app_overview);
+
+	}
+
+}
---
+++ src/com/xihuanicode/tlatoa/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.xihuanicode.tlatoa;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.xihuanicode.tlatoa.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+	private int splashDelay = 2000;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				finish();
+				Intent mainIntent = new Intent().setClass(SplashActivity.this, AppOverviewActivity.class);
+				startActivity(mainIntent);
+			}
+
+		};
+
+		Timer timer = new Timer();
+		timer.schedule(task, splashDelay);
+	}
+
+}
