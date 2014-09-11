e3b619216ab0e95d027f658ce82c94cd68b3c425
Jorge Lalo <hdez.jeduardo@gmail.com>
2014-09-11 14:18:21 -0500

    Deleted the temporary adapter used com.xihuanicode.viewpager.ImageAdapter

--- src/com/xihuanicode/tlatoa/viewpager/ComplexAdapter.java
+++ src/com/xihuanicode/tlatoa/viewpager/ComplexAdapter.java
@@ -12,14 +12,13 @@ import android.widget.TextView;
 import com.xihuanicode.tlatoa.R;
 
 public class ComplexAdapter extends PagerAdapter {
-	
+
 	private Context context;
-	
-	private int[] imagesArray = new int[]
-	        {
-				R.drawable.test_app_overview_image1,
-				R.drawable.app_overview_img_1
-			};
+
+	private int[] imagesArray = new int[] {
+			R.drawable.app_overview_img_1,
+			R.drawable.app_overview_img_0
+	};
 
 	public ComplexAdapter(Context context) {
 		this.context = context;
@@ -38,26 +37,31 @@ public class ComplexAdapter extends PagerAdapter {
 	@SuppressLint("InflateParams")
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
-		
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View vi = inflater.inflate(R.layout.viewpager_layout, null);
-		
-		ImageView imageView = (ImageView) vi.findViewById(R.id.ivViewPagerImageView);
+
+		ImageView imageView = (ImageView) vi
+				.findViewById(R.id.ivViewPagerImageView);
 		imageView.setImageResource(imagesArray[position]);
-		
+
 		TextView tv = (TextView) vi.findViewById(R.id.tvViewPagerTextView);
 		switch (position) {
 		case 0:
-			tv.setText(context.getString(R.string.tlatoa_appoverview_1st_viewpager_item_text));
+			tv.setText(context
+					.getString(R.string.tlatoa_appoverview_1st_viewpager_item_text));
 			break;
 		case 1:
-			tv.setText(context.getString(R.string.tlatoa_appoverview_2nd_viewpager_item_text));
+			tv.setText(context
+					.getString(R.string.tlatoa_appoverview_2nd_viewpager_item_text));
 			break;
 		case 2:
-			tv.setText(context.getString(R.string.tlatoa_appoverview_3rd_viewpager_item_text));
+			tv.setText(context
+					.getString(R.string.tlatoa_appoverview_3rd_viewpager_item_text));
 			break;
 		}
-		
+
 		((ViewPager) container).addView(vi, 0);
 
 		return vi;
